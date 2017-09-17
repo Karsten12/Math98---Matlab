361cea4bf041a0ab6f6a023ee3a1a220f1d59664
kfonseca <karstenfonseca@gmail.com>
2017-09-16 23:24:38 -0700

    Finished hw2

--- HW#2/assignment_2_Fonseca_Karsten.m
+++ HW#2/assignment_2_Fonseca_Karsten.m
@@ -4,9 +4,13 @@
 
 % Problem 1
 X = linspace(-39, 39, 5);
-mySin(X)
-sin(X)
-% Problem 2
+mySin(X);
+sin(X);
+a = pwError(5); %a = [1.2856, 1.1457]
+b = pwError(10); %b = [0.9975, 0.9362]
+c = pwError(20); %c = [1.0122, 1.0062]
+d = pwError(100); %d = [1.0010, 1.0005]
+
 
 
 
--- HW#2/pwError.m
+++ HW#2/pwError.m
@@ -1,16 +1,27 @@
-function result = pwError(input)
+function result = pwError(NUM)
 % Function to perform pointwise error calculations
-C = (2*pi)/101
-x = -1*pi:C:pi
-SNF_1 = zeros(5,101)
-for n = 1:5
+x = linspace(-1*pi, pi, 101);
+f = @(y) (y / abs(y));
+SNF_1 = zeros(NUM,101);
+
+for n = 1:NUM
     for m = 1:101      
-        SNF_1(n, m) = (sin(2*n+1)*x(m))/(2*n+1)
+        SNF_1(n, m) = (sin(2*n+1)*x(m))/(2*n+1);
     end
 end
 
-f = @(y) (y / abs(y))
-ERR = arrayfun(f, x)
+snf_x = SNF_1(NUM, :); 
+f_x = arrayfun(f, x);
+ERR = abs(f_x - snf_x);
+
+plot(x, ERR); hold on;
+plot(x, SNF_1);
+max_err = max(ERR);
+avg_err = mean(ERR, 2, 'omitnan');
+
+result = [max_err, avg_err];
+
+
 
 
         
